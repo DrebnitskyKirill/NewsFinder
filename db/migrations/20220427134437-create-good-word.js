025cc3b8d1a0856e73b98ec66c037e90fde569a5
Ernest Deev <erndeev@mail.ru>
2022-04-27 18:22:53 +0300

    db create

---
+++ db/migrations/20220427134437-create-good-word.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Good_words', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      word: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      count: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Good_words');
+  }
+};
---
+++ db/models/good_word.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Good_word extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User }) {
+   Good_word.belongsTo( User, { foreignKey: "user_id" })
+    }
+  }
+  Good_word.init(
+    {
+      word: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      user_id: {
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      count: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Good_word",
+    }
+  );
+  return Good_word;
+};
