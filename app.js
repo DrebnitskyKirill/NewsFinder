20d68d3be2b4d913a125a074fa34ff993cd4d811
DrebnitskyKirill <mr.drebnitzky@yandex.ru>
2022-04-28 11:27:06 +0300

    ApiCss

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-require('dotenv').config()
+require('dotenv').config();
 
 // npm
-const config = require('./config/config')
-const express = require('express')
+const express = require('express');
+const config = require('./config/config');
 
 // routers
-const mainRouter = require('./routes/main.route')
+const mainRouter = require('./routes/main.route');
 
 // app && PORT (создаем сервер)
-const app = express()
-const PORT = process.env.PORT ?? 3000
+const app = express();
+const PORT = process.env.PORT ?? 3000;
 
 // config
-config(app)
+config(app);
 
 // routes
-app.use('/', mainRouter)
+app.use('/', mainRouter);
 
 // listen
-app.listen(PORT, () => { console.log(`*** Working at PORT: ${PORT} ***`)})
+app.listen(PORT, () => { console.log(`*** Working at PORT: ${PORT} ***`); });
