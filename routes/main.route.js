2a7208f07696efb4618c29e78ecbc3583d4e22bc
Anton <anton.yasinyuk@mail.ru>
2022-04-28 19:23:53 +0300

    goodWorg push of bd and - one registration

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,8 +1,10 @@
-const router = require('express').Router();
-const { get } = require('express/lib/response');
-const { Bad_word, Good_word } = require('../db/models');
+const router = require("express").Router();
+const res = require("express/lib/response");
+const { get } = require("express/lib/response");
+const { Bad_word, Good_word } = require("../db/models");
 
-router.route('/')
+router
+  .route("/")
   .get((req, res) => {
     if (req.session.isAuthorized) {
       isAuthorized = false;
@@ -10,20 +12,31 @@ router.route('/')
       isAuthorized = true;
     }
 
-    res.render('main', { isAuthorized });
+    res.render("main", { isAuthorized });
   })
-// закидываем в бд Good_word
+  // закидываем в бд Good_word и плохое слово тоже
 
   .post(async (req, res) => {
+    const { goodWord, count, badWord } = req.body;
+    const thisWord = await Good_word.findOne({ where: { word: goodWord } });
+    const x = await thisWord.dataValues.count + 1;
+    console.log(x);
 
-    const { goodWord , count} = req.body;
-    console.log(goodWord)
-    // try{
-    //   const checkGood = await Good_word.findOne({
-    //     where:{word}
-    //   })
-    // }
-    const wordGood = await Good_word.create({ word:goodWord, count:'0', user_id: 1  });
+    if (thisWord) {
+      const wordGood = await Good_word.create({
+        word: goodWord,
+        count:x,
+        user_id: req.session.sid,
+      });
+
+      const wordBAd = await Bad_word.create({
+        word: badWord,
+        user_id: req.session.sid,
+      });
+    }
   });
+// .get((req, res) => {
+//   res.render('main')
+// })
 
 module.exports = router;
