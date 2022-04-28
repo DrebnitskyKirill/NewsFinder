aa8dd913ce344e1d8709992660eb01648f8b0fd8
Olesya <olboyarshinova@gmail.com>
2022-04-28 20:53:07 +0300

    search

---
+++ public/js/client.js
@@ -0,0 +1,9 @@
+const news = document.querySelector('#news')
+const rss = document.querySelector('.rss-items')
+// console.log(news)
+// console.log(rss)
+
+
+
+let rssWords = rss.innerText.split(' ')
+console.log(rssWords)
